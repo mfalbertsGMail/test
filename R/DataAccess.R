a38f6bd8273387f459b78619c542fb636c8f1470
Alberts, Mark Frank <malberts@deloitte.com>
2016-11-16 17:34:23 -0700

    correct issues found in code review and apply coding style detailed in DataAccess.R.

--- R/DataAccess.R
+++ R/DataAccess.R
@@ -1,39 +1,45 @@
-#' The DataAccess object is the main object that must be created in order to use any of 
-#' the other methods.  This object is always the first parameter passed to any of the 
-#' the methods.
+# Style guide:
+# 1. Variable names are all lower case use _ to separate words (i.e. day_one)
+# 2. Functions names are camel case, verbs i.e. (FIInstrumentGet)
+# 3. Functions (methods) associated with S3 classes follow use the naming convention ClassName.FunctionName(class_object_instance,...)
+
+
+#' DataAccess is a required object which always serves as the first parameter 
+#' passed to any of the other functions.
+#' 
 #' @title DataAccess: da_obj to connect and interact with the Capital database
 #' @examples
-#' connectionString = "Driver={Sql Server};server=(local);trusted_connection=Yes;database=Internal_Capital_DEV;"
-#' sc_da <- DataAccess(connection_param=connectionString, fs_id_param=1)
-#' DataAccess.fi_instrument_get(sc_da, NULL, 1)
-#' @param connection_param The SQL Server connection string
-#' @param fs_id_param The scenario ID
+#' connection_string = "Driver={Sql Server};server=(local);trusted_connection=True;database=Internal_Capital_DEV;"
+#' sc_da <- DataAccess(connection_string_param=connection_string, fs_id_param=1)
+#' DataAccess.FIInstrumentGet(sc_da, NULL, 1)
+#' @param connection_string_param - The SQL Server connection string
+#' @param fs_id_param - The scenario id
 #' @export
-DataAccess <- function(connection_param ="", fs_id_param=NULL)
+DataAccess <- function(connection_string_param = "", fs_id_param = NULL)
 {
   # save the da_obj variables
   me <- list(
-    connection = connection_param,
+    connection = connection_string_param,
     fs_id = fs_id_param,
     # locals
     is_init = 0
   )
 
-  ## set the name for the class
+  # set name for class
   class(me) <- append(class(me), "DataAccess")
   return(me)
 }
 
 
-#' Returns the connection status for current da_obj connection string
+#' Returns the connection status 
 #' 
 #' Takes a DataAccess da_obj and returns "success" if the connection is 
 #' working otherwise the error message is returned.
-#' @param da_obj - The Solvas dataAccess object 
+#' @param da_obj - Current instance of Solvas|Capital's DataAccess class.
 #' @return connection status string "success" if connection is valid otherwise an error message is returned
 #' @import RODBC
 #' @export
-DataAccess.connection_status <- function(da_obj) {
+DataAccess.ConnectionStatus <- function(da_obj) {
   return 
     tryCatch(
       {
@@ -46,31 +52,31 @@ DataAccess.connection_status <- function(da_obj) {
     )
 }
 
-#' Get fincancial instruments for the context
+#' Get financial instruments
 #' 
-#' Takes a DataAccess da_obj and effective_date or effective_period  and returns a datatable
+#' Takes a DataAccess da_obj and effective_date or effective_period and returns a DataTable
 #' with the instruments.  Properties that are schedules are coalesced 
 #' to a single value based on the effective_date or effective_period. 
-#' NOTE: EITHER effective_date or effective_period must be poplulate the other one must be NULL
-#' @param da_obj - The Solvas dataAccess object 
-#' @param effective_date = Used for to resolve schedules column values to a single scalar value
-#' @param effective_period = Used for to resolve schedules column values to a single scalar value (1=first period)
+#' NOTE: EITHER effective_date or effective_period must be populated, the other one must be NULL
+#' @param da_obj - Current instance of Solvas|Capital's DataAccess class. 
+#' @param effective_date - The date to use for schedule data types
+#' @param effective_period - The period to use for schedule data types (1=first period)
 #' @return Data frame containing the financial instrument data
 #' @import RODBC
 #' @export
-DataAccess.fi_instrument_get <- function(da_obj, effective_date = NULL, effective_period = NULL) {
-  da_obj$is_init  == 0
-    sp_fi_instrument_init(da_obj$connection,da_obj$fs_id)
+DataAccess.FIInstrumentGet <- function(da_obj, effective_date = NULL, effective_period = NULL) {
+ if (da_obj$is_init == 0)
+    SPInstrumentPropertyLoad(da_obj$connection,da_obj$fs_id)
   da_obj$is_init = 1
-  return(sp_fi_instrument_get(da_obj$connection,da_obj$fs_id, effective_date, effective_period))
+  return(SPFIInstrumentGet(da_obj$connection,da_obj$fs_id, effective_date, effective_period))
 }
 
-#' Get economic assumptions for the context
+#' Get economic assumptions
 #' 
-#' @param da_obj - The Solvas dataAccess object 
-#' @param transformation_sequence_order - an integer value found on the 'Transformations' screen
-#' @param criteria_sequence_order - an integer value found on the 'Transformation Entries for Sequence' screen
+#' @param da_obj - Current instance of Solvas|Capital's DataAccess class.
+#' @param transformation_sequence_order - An integer value found on the 'Transformations' screen
+#' @param criteria_sequence_order - An integer value found on the 'Transformation Entries for Sequence' screen
 #' @export
-DataAccess.fs_assumptions_get <- function(da_obj, transformation_sequence_order, criteria_sequence_order) {
-  return(sp_fs_assumptions_get(da_obj$connection, da_obj$fs_id, transformation_sequence_order, criteria_sequence_order))
+DataAccess.FSAssumptionsGet <- function(da_obj, transformation_sequence_order, criteria_sequence_order) {
+  return(SPFSAssumptionsGet(da_obj$connection, da_obj$fs_id, transformation_sequence_order, criteria_sequence_order))
 }
--- SC_ModelHeader.r
+++ SC_ModelHeader.r
@@ -1,11 +1,10 @@
 
-#!!!! REMOVE THIS FOR REAL HEADER
-#rm(list = ls(pattern = "sc_*")) # remove any sc_ from the env
-# declare for local testing of call from server
-#sc_is_running_from_server = 1 
-#sc_connection_string = "Driver={Sql Server};server=(local);trusted_connection=Yes;database=Internal_Capital_DEV;"
-#sc_event_id = NULL
-#sc_fs_id = 1
+#  !!!! REMOVE THIS FOR REAL HEADER
+#  rm(list = ls(pattern = "sc_*")) # remove all variables that begin with sc_
+#  # declare for local testing of call from serversc_is_running_from_server = 1 
+#  sc_connection_string = "Driver={Sql Server};server=(local);trusted_connection=True;database=Internal_Capital_DEV;"
+#  sc_event_id = NULL
+#  sc_fs_id = 1
 #end 
 ###################################################################################################
 # START OF SOLVAS|CAPITAL HEADER - DO NOT MODIFY 
@@ -13,12 +12,12 @@
 # Variables that start with sc_ should be considered 'Solvas|Capital' system variables. Naming user
 # variables with sc_ should be avoided.
 #
-# System variables passed from the Server as parameters when running a scenario:
+# System variables passed from the server as parameters when running a scenario:
 #
 # sc_connection_string      - string = the connection string used connect back to the sever
 # sc_is_running_from_server - bit - 1 = this script is being called from the server
 #                                   0 = local development (or undefined)
-# sc_fs_id                  - int - the ID of the scenario being processed.
+# sc_fs_id                  - int - the id of the scenario being processed.
 # sc_event_id               - int - used to report event messages to server 
 # 
 ###################################################################################################
@@ -29,22 +28,22 @@ if (exists('sc_is_running_from_server') && sc_is_running_from_server == 1)
 {
   # declare diagnostic variables 
   sc_undefined = "undefined"
-  sc_da <- DataAccess(connection_param=sc_connection_string, fs_id_param=sc_fs_id)
-  sc_da_connection_status <- DataAccess.connection_status(sc_da)
+  sc_da <- DataAccess(connection_param = sc_connection_string, fs_id_param = sc_fs_id)
+  sc_da_connection_status <- DataAccess.ConnectionStatus(sc_da)
   # collect all the variables in the environment that have sc_ prefix
-  sc_var_name = c(unlist(ls(pattern = "sc_*"),use.names = FALSE))
+  sc_var_name = c(unlist(ls(pattern = "sc_*"), use.names = FALSE))
   # set vector with variable values, use sc_undefined if variable does not exist - get0 checks if the var name is a variable ifnotfound is a parameter name to get0
   sc_var_value = unname(sapply(sc_var_name,
                              function(x) 
                              {  
-                               ifelse(is.null(get0(x,ifnotfound=sc_undefined)), 
+                               ifelse(is.null(get0(x, ifnotfound = sc_undefined)), 
                                       sc_undefined, 
-                                      toString(get0(x,ifnotfound=sc_undefined)))
+                                      toString(get0(x, ifnotfound = sc_undefined)))
                              }))
   sc_result_set = data.frame(sc_var_name, sc_var_value, stringsAsFactors = FALSE)
 
   print(sc_result_set)
-  # push the results to the SQL server parameter from sp_execute_external_script
+  # Push results to the SQL Server parameter
   sc_output_table <- as.data.frame(sc_result_set)
 } else
   
@@ -54,7 +53,7 @@ if (exists('sc_is_running_from_server') && sc_is_running_from_server == 1)
   
   ###################################################################################################
   #
-  # Note: For local model development modifiy the variables in this statement to point the database
+  # Note: For local model development, modify the variables in this statement to point to the database
   # (sc_connection_string) and scenario ID (sc_fs_id)
   #
   ###################################################################################################
@@ -63,14 +62,18 @@ if (exists('sc_is_running_from_server') && sc_is_running_from_server == 1)
   sc_connection_string = "Driver={Sql Server};server=(local);trusted_connection=Yes;database=Internal_Capital_DEV;"
   sc_event_id = NULL
   sc_fs_id = 1
-  sc_da <- DataAccess(connection_param=sc_connection_string, fs_id_param=sc_fs_id)
-  sc_da_connection_status <- DataAccess.connection_status(sc_da)
+  sc_da <- DataAccess(connection_string_param = sc_connection_string, fs_id_param = sc_fs_id)
+  sc_da_connection_status <- DataAccess.ConnectionStatus(sc_da)
 
 }
  # Example...
 if (sc_da_connection_status == "success") {
   # do model development here...
-  instruments = DataAccess.fi_instrument_get(sc_da,NULL,1)
+  instruments = DataAccess.FIInstrumentGet(sc_da,NULL,1)
   # print out interest_rate_effective value for all loans
   print(instruments['interest_rate_effective'])
+  instruments = DataAccess.FIInstrumentGet(sc_da,'9/30/2014',NULL)
+  # print out interest_rate_effective value for all loans
+  print(instruments['interest_rate_effective'])
+
 }  
