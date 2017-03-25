cbde69ee93fe235eb7f94be21d3db7c8687e3ef6
Mark Alberts <malberts@deloitte.com>
2017-03-25 07:59:45 -0600

    Removed dependencies on plyr, stringr Rcpp

--- R/StoredProcedures.R
+++ R/StoredProcedures.R
@@ -34,9 +34,6 @@ sqlIsNullStr <- function(string_value, replacement_value = 'NULL') {
 #' 
 #' @param data - dataframe returned from a sql call 
 #' @return throws status_message if status is CERR
-#' Try importing these packages here - they seem to be required by some package I'm using...
-#' @import plyr
-#' @import stringr
 #' @keywords internal
 SPResultCheck <- function(data) {
   if (is.null(data[['r_status_code']]) == FALSE & is.null(data$r_status_code) == FALSE){
